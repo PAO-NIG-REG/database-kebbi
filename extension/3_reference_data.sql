b910d89be59eabee70a1ec1229e2f55fb8ac033f
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2015-09-11 13:48:51 +0200

    corrected scripts causing errors

--- extension/3_reference_data.sql
+++ extension/3_reference_data.sql
@@ -1,7 +1,7 @@
 
-INSERT INTO request_type (code, request_category_code, display_value, description, status, nr_days_to_complete, base_fee, area_base_fee, value_base_fee, nr_properties_required, notation_template, rrr_type_code, type_action_code, service_panel_code, display_group_code, display_order) VALUES ('subdivideProperty', 'registrationServices', 'SubDivision of Property (General)::::???????? ????? (?????)::::????? ?? (???)::::Varier Droit (Général)::::::::Variar Direitos (Geral)::::???? (??)', '...::::...::::...::::...::::::::...::::...', 'c', 5, 5.00, 0.00, 0.000, 1, 'Variation of <right> <reference>', NULL, 'vary', 'property', 'generalReg', 180);
+INSERT INTO application.request_type (code, request_category_code, display_value, description, status, nr_days_to_complete, base_fee, area_base_fee, value_base_fee, nr_properties_required, notation_template, rrr_type_code, type_action_code, service_panel_code, display_group_code, display_order) VALUES ('subdivideProperty', 'registrationServices', 'SubDivision of Property (General)::::???????? ????? (?????)::::????? ?? (???)::::Varier Droit (Général)::::::::Variar Direitos (Geral)::::???? (??)', '...::::...::::...::::...::::::::...::::...', 'c', 5, 5.00, 0.00, 0.000, 1, 'Variation of <right> <reference>', NULL, 'vary', 'property', 'generalReg', 180);
 
-INSERT INTO request_type_requires_source_type (source_type_code, request_type_code) VALUES ('deed', 'subdivideProperty');
+INSERT INTO application.request_type_requires_source_type (source_type_code, request_type_code) VALUES ('deed', 'subdivideProperty');
 
 UPDATE application.request_type
 SET		base_fee= '200',
