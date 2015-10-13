49f445cc34eeeadacd03d625e9d34703fb4e6705
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2015-10-13 17:34:47 +0200

    adaptation between ot and sola codes

--- extension/3_reference_data.sql
+++ extension/3_reference_data.sql
@@ -1,5 +1,8 @@
-
-INSERT INTO application.request_type (code, request_category_code, display_value, description, status, nr_days_to_complete, base_fee, area_base_fee, value_base_fee, nr_properties_required, notation_template, rrr_type_code, type_action_code, service_panel_code, display_group_code, display_order) VALUES ('subdivideProperty', 'registrationServices', 'SubDivision of Property (General)::::???????? ????? (?????)::::????? ?? (???)::::Varier Droit (Général)::::::::Variar Direitos (Geral)::::???? (??)', '...::::...::::...::::...::::::::...::::...', 'c', 5, 5.00, 0.00, 0.000, 1, 'Variation of <right> <reference>', NULL, 'vary', 'property', 'generalReg', 180);
+UPDATE source.administrative_source_type
+   SET display_value='Diagram Image'
+ WHERE code='cadastralSurvey';
+ 
+INSERT INTO application.request_type (code, request_category_code, display_value, description, status, nr_days_to_complete, base_fee, area_base_fee, value_base_fee, nr_properties_required, notation_template, rrr_type_code, type_action_code, service_panel_code, display_group_code, display_order) VALUES ('subdivideProperty', 'registrationServices', 'SubDivision of Property (General)::::???????? ????? (?????)::::????? ?? (???)::::Varier Droit (GÃ©nÃ©ral)::::::::Variar Direitos (Geral)::::???? (??)', '...::::...::::...::::...::::::::...::::...', 'c', 5, 5.00, 0.00, 0.000, 1, 'Variation of <right> <reference>', NULL, 'vary', 'property', 'generalReg', 180);
 
 INSERT INTO application.request_type_requires_source_type (source_type_code, request_type_code) VALUES ('deed', 'subdivideProperty');
 
