fabf58a0b89861c7c2029bbc17baa03ca72c4ed1
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2015-10-15 11:47:09 +0200

    updated automatization scripts for database scripts generation

--- extension/3_reference_data.sql
+++ extension/3_reference_data.sql
@@ -1,10 +1,12 @@
+DELETE FROM application.request_type  where  WHERE code = 'subdivideProperty';
+INSERT INTO application.request_type (code, request_category_code, display_value, description, status, nr_days_to_complete, base_fee, area_base_fee, value_base_fee, nr_properties_required, notation_template, rrr_type_code, type_action_code, service_panel_code, display_group_code, display_order) VALUES ('subdivideProperty', 'registrationServices', 'SubDivision of Property (General)::::???????? ????? (?????)::::????? ?? (???)::::Varier Droit (Général)::::::::Variar Direitos (Geral)::::???? (??)', '...::::...::::...::::...::::::::...::::...', 'c', 5, 5.00, 0.00, 0.000, 1, 'Variation of <right> <reference>', NULL, 'vary', 'property', 'generalReg', 180);
+
+DELETE FROM application.request_type_requires_source_type   where  WHERE request_type_code = 'subdivideProperty';
+INSERT INTO application.request_type_requires_source_type (source_type_code, request_type_code) VALUES ('deed', 'subdivideProperty');
+
 UPDATE source.administrative_source_type
    SET display_value='Diagram Image'
  WHERE code='cadastralSurvey';
- 
-INSERT INTO application.request_type (code, request_category_code, display_value, description, status, nr_days_to_complete, base_fee, area_base_fee, value_base_fee, nr_properties_required, notation_template, rrr_type_code, type_action_code, service_panel_code, display_group_code, display_order) VALUES ('subdivideProperty', 'registrationServices', 'SubDivision of Property (General)::::???????? ????? (?????)::::????? ?? (???)::::Varier Droit (Général)::::::::Variar Direitos (Geral)::::???? (??)', '...::::...::::...::::...::::::::...::::...', 'c', 5, 5.00, 0.00, 0.000, 1, 'Variation of <right> <reference>', NULL, 'vary', 'property', 'generalReg', 180);
-
-INSERT INTO application.request_type_requires_source_type (source_type_code, request_type_code) VALUES ('deed', 'subdivideProperty');
 
 UPDATE application.request_type
 SET		base_fee= '200',
