ff8ae461e6e19126552d534d98d2c132983a7333
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2015-10-29 18:11:42 +0100

    refactored report package for katsina and corrections on reference data script

--- extension/9_integration_OT_SOLAREG.sql
+++ extension/9_integration_OT_SOLAREG.sql
@@ -46,3 +46,47 @@ UPDATE administrative.ba_unit_detail  SET detail_code='cOfOtype' WHERE detail_co
 UPDATE administrative.ba_unit_detail  SET detail_code='yearlyRent' WHERE detail_code='rent';
 UPDATE administrative.ba_unit_detail  SET detail_code='reviewPeriod' WHERE detail_code='revperiod';
 UPDATE administrative.ba_unit_detail  SET detail_code='dateCommenced' WHERE detail_code='startdate';
+
+UPDATE administrative.ba_unit_detail_type
+   SET  status='c',
+       order_view=16
+ WHERE code='estate';
+ 
+UPDATE administrative.ba_unit_detail_type
+   SET  status='c',
+       order_view=17
+ WHERE code='zone';
+
+UPDATE administrative.ba_unit_detail_type
+   SET  status='x',
+       order_view=18
+ WHERE code='LGA';
+
+UPDATE administrative.ba_unit_detail_type
+   SET  status='x',
+       order_view=19
+ WHERE code='IntellMapSheet';
+
+UPDATE administrative.ba_unit_detail_type
+   SET  status='x',
+       order_view=20
+ WHERE code='instrumentRegistrationNo';
+
+
+UPDATE system.setting 
+   SET vl = 'KE'
+ WHERE name = 'system-id';
+ 
+
+UPDATE system.br
+  SET technical_description = 'It accepts parameters: 
+  #{estate} = G – means that the land is granted by the Government, but is might be P , which means that the land is private , or customary owned land
+  #{zone} = first two letters of the zonal area name 
+  ' 
+  WHERE id = 'generate-cofo-nr';
+
+
+UPDATE system.br_definition
+  SET body = 'SELECT #{zone} || ''/'' || #{estate} || ''/'' ||  to_char(now(), ''yymm'') || trim(to_char(nextval(''administrative.cofo_nr_seq''), ''0000'')) AS vl'
+WHERE br_id = 'generate-cofo-nr';
+  
