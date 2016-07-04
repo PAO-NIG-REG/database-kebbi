441bc9d14df75ebaf7226685792c82b1b79c375f
unknown <rizzom@NRC016.hq.un.fao.org>
2016-07-04 12:20:33 +0200

    ticket 24 and 26

---
+++ changeset/1606a_86.sql
@@ -0,0 +1,100 @@
+﻿
+
+
+delete from cadastre.zone_type;
+
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('ALR','ALERO', '','c');    
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('AR','ARGUNGU', '','c');
+    
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('AUG','AUGIE', '','c');
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('AW','AREWA', '','c');
+
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('BGD','BAGUDO', '','c');    
+    
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('BI','BIRNIN', '','c');
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('BU','BUNZA', '','c');    
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('DD','KAMBA', '','c');
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('DKG','DAKINGARI', '','c');    
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('DW','DANKO WASAGU', '','c');    
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('FAK','FAKAI', '','c');    
+    
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('GW','GWANDI', '','c');
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('KB','IKOKO BESSE', '','c');    
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('KG','KALGO', '','c');
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('JE','JEGA', '','c');    
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('MY','MYIAWA', '','c');    
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('NG','NGASGI', '','c');  
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('SAK','SAKABA', '','c'); 
+ 
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('SNA','SHANGA', '','c');    
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('SU','SURU', '','c'); 
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('YAU','YAGIRI', '','c');    
+
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('WRR','WARRAN', '','c');  
+           
+INSERT INTO cadastre.zone_type(
+            code, display_value, description, status)
+    VALUES ('ZU','ZURU', '','c');    
+  
+
+ 
