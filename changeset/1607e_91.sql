1008d9c440236c5b664d4882b66bff11100d8bd0
unknown <RizzoM@NRC016.hq.un.fao.org>
2016-07-20 17:38:45 +0200

    default for lease condition template

---
+++ changeset/1607e_91.sql
@@ -0,0 +1,89 @@
+﻿--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.4.4
+-- Dumped by pg_dump version 9.4.4
+-- Started on 2016-07-20 16:21:01
+
+SET statement_timeout = 0;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = administrative, pg_catalog;
+
+--
+-- TOC entry 4159 (class 0 OID 73505)
+-- Dependencies: 435
+-- Data for Name: lease_condition_template; Type: TABLE DATA; Schema: administrative; Owner: postgres
+--
+
+INSERT INTO lease_condition_template VALUES ('3d29b003-76c2-4edd-8c6f-983563a67e67', 'BuildingSite', NULL, '
+1.	To pay in advance without demand to the Governor, or other person appointed by him, the proportion of the rent stated in the certificate (proportion of rent) applicable in the first year of this right of occupancy from the commencement date stated in the certificate (commencement date) and then the annual rent as stated in the certificate (annual rent) payable on the first day of January each year.
+2.	To pay and discharge all rates, taxes, assessments and impositions whatsoever which shall at any time be charged, assessed or imposed on the said land or any part thereof or any building thereon, or upon the occupier or occupiers thereof.
+2.1	The Governor may as near as it is convenient to the expiration of a rent review period stated in the certificate (rent review period), revise the rent and fix a revised rent which shall apply for the next rent review period or if the term, as stated in the certificate (term) of the right of occupancy falls within the next rent review period, the remainder of the term.
+If the Governor does revise the rent, he shall cause a notice in writing to be sent to the right holder(s) informing them of the revision of the rent. 
+The right holder(s) shall thereupon within one month of the date of the receipt of such a notice, or within seven days of the commencement of the period for which the rent has been revised, whichever shall last happen, without further demand, pay to the Governor or to other persons appointed by him the additional rent owing for the current year.
+Thereafter the right holders shall pay in advance on the first day of January in each year without demand to the Governor or other persons appointed by him in lieu the latest revised rent
+2.2	It shall be lawful for the Governor at any time to re-enter upon the said land or any part thereof in the name of the whole and henceforth hold and enjoy the same as if the right of occupancy had not been granted should the following situations arise:
+(i)	If the yearly rent for the time being payable in respect of the said land or any part thereof shall be in arrears for the space of three months, whether the same or shall not have been legally demanded;
+(ii)	If the right holder(s) become bankrupt or make a composition with creditors or enter into liquidation, whether compulsory or voluntary; or
+(iii)	If there shall be any breach or non-observance of any of the occupier’s covenants or agreements herein contained.
+Any such exercise of the power to re-enter shall be without prejudice to any right of action or remedy of the Governor for any antecedent breach of  covenant by the right holder(s).
+3.	To pay forthwith without demand to the Governor or other person appointed by him on the issue of the certificate of occupancy (if not sooner paid) all survey fees, registration fees and other charges due in respect of the preparation and issue and registration of the certificate of occupancy.
+4.	To erect and complete on the said land the buildings and other works specified in detailed plans approved or to be approved by the officer appointed by the Governor. These buildings and other works to be erected and completed in accordance with the approved plans to the satisfaction of the officer appointed by the Governor. From the date of the commencement of the right of occupancy, as stated in the certificate  (commencement date), such work to be completed within the period of time if stated in the certificate (period to complete works)
+The value of the buildings and other works, if stated in the certificate (value of works) shall be not less than the stated value.
+5.	To maintain in good substantial repair to the satisfaction of the said or other officer appointed by the Governor, all buildings on the said land (whether now erected or to be erected in pursuance of other stated conditions)
+6.	To clear and keep clear the said land of stagnant water, long grass, rank weeds and bush and accumulations and deposits of rubbish and other unwholesome matter, and to keep the same in all respects in a clean and sanitary condition, and for such purposed to do and execute all such acts and works as the Governor or any officer authorised  by him may reasonably require.
+7.	To conform to all rules laid down from time to time in regard to the location of buildings, refuse pits and latrines.
+8.	The land shall not be unoccupied for any period exceeding six months at any one time, or eight months in any year from the commencement of this right of occupancy as stated in the certificate (commencement date) unless an alternative period of absence is stated in the certificate (permissible period of absence). 
+9.	Upon the expiration of the term of this right of occupancy, to render up to the Governor in good and substantial repair to the satisfaction of the officers appointed by the Governor all buildings on the said land erected in pursuance hereof which have not been removed with the consent of the Governor and all buildings already upon the said land at the commencement of the said term of the right of occupancy, as stated in the certificate (commencement date) which have not been removed. 
+10.	Not to erect or build or permit to be erected or built on the said land any buildings other than those covenanted to be erected by virtue to the certificate of occupancy nor to make or permit to be made any addition or alteration to the said buildings to be erected or buildings already erected on the land except in accordance with plans and specifications approved by the said or other officer appointed by the Governor on his behalf.
+11.	Not to alienate the right of occupancy hereby granted or any part thereof by sale, assignment, mortgage, transfer or possession, sub-lease or bequest or otherwise however without the consent of the Governor having first had been obtained.
+12.	To use the said land only for the purpose as stated in the certificate (permitted land use).
+', '8dbab555-1aef-4627-ab81-c126dd71c91a', 1, 'i', 'test', '2016-07-20 15:17:37.127');
+INSERT INTO lease_condition_template VALUES ('d31f4160-f8ec-412b-9133-47e924db44a9', 'Agriculture', NULL, '1
+(1)	To pay in advance without demand to the Governor, or other person appointed by him, the proportion of the rent stated in the certificate (proportion of rent) applicable in the first year of this right of occupancy from the commencement date stated in the certificate (commencement date) and then the annual rent as stated in the certificate (annual rent) payable on the first day of January each year.
+(2)	To pay and discharge all rates, taxes, assessments and impositions whatsoever which shall at any time be charged, assessed or imposed on the said land or any part thereof or any building thereon, or upon the occupier or occupiers thereof. 
+(3)	To pay such compensation as may be fixed by the Governor or any such person authorized by him for the disturbance of native occupation and for such timber, rubber and oil palm trees or other economic  products as may be growing on the said land at the date of this certificate. 
+(4)	To bring the farmable portion of the said land progressively under cultivation pasture or other agricultural use to the satisfaction of the Permanent Secretary, Ministry of Agriculture or other officer authorised by him at an annual rate of not less than one-eighth of the said portion until the whole of the said land portion is in cultivation pasture or use.
+(5)	To take such steps as the Divisional Engineer may from time to time require for the purpose of protecting the said land from damage by erosion and to refrain from any action.
+(6)	To enclose and fence securely any part of the said land as may be used for the herding, rearing, stalling or grazing of livestock in order safely to contain them at all times therein and for this purpose to erect and maintain such enclosing fences, walls or palisades and take such other measures as may be required by the Divisional Engineer.
+(7)	On demand to pay such compensation as may be assessed by the Divisional Engineer or by an Local Authority acting on behalf, on account of any damage or injury done to persons, animals, buildings, trees or crops that shall be caused by any omission to keep livestock confined in the manner provided by other clauses in this certificate of occupancy. Such demands are subject to a right of appeal to the Commissioner whose decision shall be final against any award of compensation made by the Divisional Engineer or Local Authority in respect of such damage or injury.
+(8)	Should livestock be brought on the said land to take such measures as the Divisional Engineer may direct to prevent over stocking or other damage to the said land by the said livestock.
+(9)	(a) (Subject to the provisions of paragraph 9(b), not to construct upon the said land any dwelling, houses or any permanent erections except for labourers'' huts and buildings designed for storing  agricultural machinery, tools or produce of for other purposes directly connected with the carrying on of cultivation, planting or farming or the housing of livestock except as specifically allowed for in other clauses in this certificate.
+(b) To erect buildings and or other works to the satisfaction of the Governor (or other officer appointed by him) and within the specified period  from the commencement of this right of occupancy. The specified period is -::::::::::::::::::::::::. 
+(10)	Unless authorised in writing thereto by the Divisional Engineer, not to plant any trees, shrubs or crops and not to erect any building within one hundred  feet of the centre line of any main road.
+(11)	Not without the written consent of the Governor or any other person authorised by him, to pollute, dam or divert or in any way interfere directly or indirectly with any stream, pond, river or lake.
+(12)	To take such measures as the Divisional Engineer may require to prevent the contamination of any water supply arising by reason of the occupation by the right holder(s) of the said land or of any operation thereon by the said right holder(s) whether such supply be for human or animal consumption or for the irrigation of any land.
+(13)	Upon the expiration of the said term to render up to the Minister in good and substantial repair to the satisfaction of the Minister (or officer appointed by him/her), all buildings on the said land erected in pursuance hereof which have not been removed with the consent of the Governor and all buildings already upon the said land at the commencement of the said term which have not been removed.
+(14)	To clear and keep clear the said land of stagnant water, long grass, rank weeds, bush, accumulations and deposits of rubbish and other unwholesome matter and generally to keep the same and all buildings erected in a clean and sanitary condition to the satisfaction of the Health Officer and for such purposes to and execute all such acts and works as the Health Officer or any other officer authorised by him may reasonably require.
+(15)	Not to alienate the right of occupancy hereby granted or any part thereof by sale, mortgage, transfer of possession, sub-lease or bequest or otherwise howsoever without the consent of the Governor first had and obtained.
+(16)	To use the said land only for the purpose as stated in this certificate (Permitted Land Use).
+(17)	To erect all buildings, structures or other fixtures in connection with animal rearing or slaughtering (as identified in the previous clause) therewith according to plans and specifications that shall be approved and subject to any conditions that may be imposed by the Health Officer.
+(18)	To take such measures as the Health Officer may prescribe for the disposal of effluents and waste products and for the prevention of the contamination of any water supply. Whether such supply be for human or animal consumption or for the irrigation or cultivation of any land.
+2.
+(1)	The Commissioner may, as near as it is convenient to the expiration of a rent review period stated in the certificate (rent review period), revise the rent and fix a revised rent which shall apply for the next rent review period or if the term, as stated in the certificate (term) of the right of occupancy falls within the next rent review period, the remainder of the term.
+If the Governor shall so revise the rent, he shall cause a notice in writing to be sent to the right holder(s) informing them of the revision of the rent. 
+The right holder(s) shall thereupon within one month of the date of the receipt of such a notice, or within seven days of the commencement of the period for which the rent has been revised, whichever shall last happen, without further demand, pay to the Accountant-General or other persons appointed by the Governor the additional rent owing for the current year.
+Thereafter the right holders shall pay in advance on the first day of January in each year without demand to the Accountant-General or other persons appointed by the Governor in lieu the latest revised rent
+(2)	It shall be lawful for the Governor at any time to re-enter upon the said land or any part thereof in the name of the whole and henceforth hold and enjoy the same as if the right of occupancy had not been granted should the following situations arise:
+(i)	if the yearly rent for the time being payable in respect of the said land or any part thereof shall be in arrears for the space of three months, whether the same or shall not have been legally demanded;
+(ii)	if the right holder(s) become bankrupt or make a composition with creditors or enter into liquidation, whether compulsory or voluntary; or
+(iii)	if there shall be any breach or non-observance of any of the occupier’s covenants or agreements herein contained.
+Any such exercise of the power to re-enter shall be without prejudice to any right of action or remedy of the Governor for any antecedent breach of covenant by the right holder(s).
+
+', '8105dc91-b47c-40c7-8b0b-eb6ae7af5f8f', 1, 'i', 'test', '2016-07-20 15:18:44.785');
+
+
+-- Completed on 2016-07-20 16:21:01
+
+--
+-- PostgreSQL database dump complete
+--
+
+
+INSERT INTO system.version SELECT '1607e' WHERE NOT EXISTS (SELECT version_num FROM system.version WHERE version_num = '1607e');
